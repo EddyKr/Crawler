5279e4739c800cb6da4add8f1cbe4a129b37c3f1
Eddy <edgarskruuze@gmail.com>
2018-06-17 18:46:14 +0200

    Web crawler test methods

---
+++ src/test/java/WebCrawlerTest.java
@@ -0,0 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class WebCrawlerTest {
+    @Before
+    public void initialization(){
+    }
+
+    @Test
+    public void shouldGetItemsQueue() {
+
+    }
+
+    @Test
+    public void shouldGetLinkList() {
+
+    }
+
+    @Test public void shouldCrawlTest () {
+
+    }
+
+    @Test
+    public void shouldReadUrl() {
+
+    }
+}
