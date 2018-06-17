7007faa6a9749a20494c7072564346d925d81230
Eddy <edgarskruuze@gmail.com>
2018-06-17 19:38:22 +0200

    Web scraper classes added

---
+++ src/main/java/WebScraper.java
@@ -0,0 +1,2 @@
+public class WebScraper {
+}
---
+++ src/test/java/WebScraperTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+import static org.mockito.Mockito.mock;
+
+public class WebScraperTest {
+    private WebScraper webScraper = mock(WebScraper.class);
+
+    @Before
+    public void initialization(){
+    }
+
+
+}
