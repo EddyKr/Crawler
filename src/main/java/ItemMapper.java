7e4c0325af4a8a498753b902e4dde85dd49800e8
Eddy <edgarskruuze@gmail.com>
2018-06-17 21:49:15 +0200

    Item mapping implemented

--- src/main/java/ItemMapper.java
+++ src/main/java/ItemMapper.java
@@ -1,4 +1,9 @@
 import org.jsoup.nodes.Document;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class ItemMapper {
 
@@ -8,7 +13,24 @@ public class ItemMapper {
      * @param  doc  document object with Item information
      */
     public Item mapItem(Document doc){
-        return null;
+
+        String category = doc.getElementsByTag("tbody").first().getElementsByTag("td").first().html();
+
+        Item item = null;
+
+        switch(category){
+            case "Books":
+                item = createBookObject(doc);
+                break;
+            case "Movies":
+                item = createMovieObject(doc);
+                break;
+            case "Music":
+                item = createMusicObject(doc);
+                break;
+        }
+
+        return item;
     }
 
     /**
@@ -17,7 +39,26 @@ public class ItemMapper {
      * @param  doc  document object with Item information
      */
     public Book createBookObject(Document doc){
-        return null;
+
+        //Map book into an object
+        String title = doc.getElementsByTag("h1").first().html();
+
+        Element table = doc.select("div.media-details").select("table").get(0);
+        Elements rows = table.select("tr");
+
+        String category = rows.get(0).select("td").html();
+        String genre = rows.get(1).select("td").html();
+        String format = rows.get(2).select("td").html();
+        Integer year = Integer.parseInt(rows.get(3).select("td").html());
+        String authors = rows.get(4).select("td").html();
+        String publisher = rows.get(5).select("td").html();
+        String isbn = rows.get(6).select("td").html();
+
+        Book bookItem = new Book(title, category, genre, format, year, authors, publisher, isbn);
+
+        System.out.println(table);
+
+        return bookItem;
     }
 
     /**
@@ -26,7 +67,25 @@ public class ItemMapper {
      * @param  doc  document object with Item information
      */
     public Movie createMovieObject(Document doc){
-        return null;
+        //Map movie into an object
+        String title = doc.getElementsByTag("h1").first().html();
+
+        Element table = doc.select("div.media-details").select("table").get(0);
+        Elements rows = table.select("tr");
+
+        String category = rows.get(0).select("td").html();
+        String genre = rows.get(1).select("td").html();
+        String format = rows.get(2).select("td").html();
+        Integer year = Integer.parseInt(rows.get(3).select("td").html());
+        String directors = rows.get(4).select("td").html();
+        String writers = rows.get(5).select("td").html();
+        String stars = rows.get(6).select("td").html();
+
+        Movie movieItem = new Movie(title, category, genre, format, year, directors, writers, stars);
+
+        System.out.println(table);
+
+        return movieItem;
     }
 
     /**
@@ -35,6 +94,22 @@ public class ItemMapper {
      * @param  doc  document object with Item information
      */
     public Music createMusicObject(Document doc){
-        return null;
+        //Map music into an object
+        String title = doc.getElementsByTag("h1").first().html();
+
+        Element table = doc.select("div.media-details").select("table").get(0);
+        Elements rows = table.select("tr");
+
+        String category = rows.get(0).select("td").html();
+        String genre = rows.get(1).select("td").html();
+        String format = rows.get(2).select("td").html();
+        Integer year = Integer.parseInt(rows.get(3).select("td").html());
+        String artist = rows.get(4).select("td").html();
+
+        Music musicItem = new Music(title, category, genre, format, year, artist);
+
+        System.out.println(table);
+
+        return musicItem;
     }
 }
