cef826653c5b4260c9a4a33556f85b24105c591a
Minda Bartosz <mindabartosz@gmail.com>
2018-06-17 20:46:49 +0200

    Add test for Item class.

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -1,12 +1,15 @@
-import org.jsoup.nodes.Document;
+import netscape.javascript.JSObject;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
-
+import static org.mockito.Mockito.when;
 
 public class ItemTest {
     private Item itemTest= mock(Item.class);
+    private JSObject jsonObject = mock(JSObject.class);
 
     @Before
     public void initialization(){
@@ -14,7 +17,14 @@ public class ItemTest {
 
     @Test
     public void shouldReturnJson() {
+        //arrange
+        when(itemTest.returnAsJSON()).thenReturn(jsonObject);
 
+        //act
+        JSObject actual = itemTest.returnAsJSON();
+
+        //assert
+        assertThat(actual, instanceOf(JSObject.class));
     }
 
 }
